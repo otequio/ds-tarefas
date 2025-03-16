f36981fddd76529fc58501292524df68581a8c2e
Cesar Rovai <cesar.arovai@gmail.com>
2025-03-16 01:19:46 -0300

    fix: tratando serializalização de datetime com record

--- backend/Application/Models/AtualizarTarefaRequest.cs
+++ backend/Application/Models/AtualizarTarefaRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Enums;
+using Infrastructure.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
@@ -13,6 +14,7 @@ namespace Application.Models
         string Titulo,
         [MaxLength(400, ErrorMessage = $"A descrição pode ter no máximo 400 caracteres")]
         string? Descricao,
+        [property: JsonConverter(typeof(NullableDateTimeConverter))]
         DateTime? DataConclusao,
         [property: JsonConverter(typeof(JsonStringEnumConverter))]
         EnumStatus Status  = EnumStatus.Pendente
---
+++ backend/Infrastructure/Serialization/NullableDateTimeConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Serialization
+{
+    public class NullableDateTimeConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var stringValue = reader.GetString();
+                if (string.IsNullOrEmpty(stringValue))
+                {
+                    return null;
+                }
+                if (DateTime.TryParse(stringValue, out var date))
+                {
+                    return date;
+                }
+            }
+            else if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+
+            throw new JsonException("Valor inválido para o campo data.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value);
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+        }
+    }
+}
